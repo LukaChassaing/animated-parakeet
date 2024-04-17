f56decc66c11eef726f01a3d54545e962b9500ba
Luka Chassaing <contact@luka-chassaing.fr>
2024-04-17 14:38:51 +0200

    Update gestion_porte_poules.cpp

--- gestion_porte_poules.cpp
+++ gestion_porte_poules.cpp
@@ -20,183 +20,190 @@
 
 using namespace std;
 
-int pinRelai_1 = 4;
-int pinRelai_2 = 5;
-int duree_ouverture = 90000000;
-int duree_fermeture = 90000000;
+const int PIN_RELAI_1 = 4;
+const int PIN_RELAI_2 = 5;
+const int DUREE_OUVERTURE = 90000000;
+const int DUREE_FERMETURE = 90000000;
 
-
-void pousse_verrin(){
-	std::cout << "Pousse verrin\n";
-	digitalWrite(pinRelai_1, 1);
-    digitalWrite(pinRelai_2, 0);
+// Fonction pour pousser le verrin
+void pousserVerrin() {
+    cout << "Pousse verrin\n";
+    digitalWrite(PIN_RELAI_1, 1);
+    digitalWrite(PIN_RELAI_2, 0);
 }
 
-void tire_verrin(){
-	std::cout << "Tire verrin\n";
-	digitalWrite(pinRelai_1, 0);
-    digitalWrite(pinRelai_2, 1);
+// Fonction pour tirer le verrin
+void tirerVerrin() {
+    cout << "Tire verrin\n";
+    digitalWrite(PIN_RELAI_1, 0);
+    digitalWrite(PIN_RELAI_2, 1);
 }
 
-void coupe_alimention_verrin(){
-	std::cout << "Coupe alimentation verrin\n";
-	digitalWrite(pinRelai_1, 0);
-    digitalWrite(pinRelai_2, 0);
+// Fonction pour couper l'alimentation du verrin
+void couperAlimentationVerrin() {
+    cout << "Coupe alimentation verrin\n";
+    digitalWrite(PIN_RELAI_1, 0);
+    digitalWrite(PIN_RELAI_2, 0);
 }
 
-void ouverture_porte(){
-	std::cout << "\nOuverture porte\n";
-	tire_verrin();
-	usleep(duree_ouverture);
-	coupe_alimention_verrin();
+// Fonction pour ouvrir la porte
+void ouvrirPorte() {
+    cout << "\nOuverture porte\n";
+    tirerVerrin();
+    usleep(DUREE_OUVERTURE);
+    couperAlimentationVerrin();
 }
 
-void fermeture_porte(){
-	std::cout << "\nFermeture porte\n";
-	pousse_verrin();
-	usleep(duree_fermeture);
-	coupe_alimention_verrin();
+// Fonction pour fermer la porte
+void fermerPorte() {
+    cout << "\nFermeture porte\n";
+    pousserVerrin();
+    usleep(DUREE_FERMETURE);
+    couperAlimentationVerrin();
 }
 
-std::vector<std::string> explode(char delim, std::string const & s)
-{
-    std::vector<std::string> result;
-    std::istringstream iss(s);
+// Fonction pour diviser une chaîne de caractères en fonction d'un délimiteur
+vector<string> explode(char delim, const string& s) {
+    vector<string> result;
+    istringstream iss(s);
 
-    for (std::string token; std::getline(iss, token, delim); )
-    {
-        result.push_back(std::move(token));
+    for (string token; getline(iss, token, delim); ) {
+        result.push_back(move(token));
     }
 
     return result;
 }
 
-int main(void)
-{
-    std::cout << "Lancement du programme de gestion de la porte des poules\n";
-	if(wiringPiSetup() < 0){
-		std::cout << "Impossible d'initialiser wiringPi\n";
-		return 1;
-	}
+int main() {
+    cout << "Lancement du programme de gestion de la porte des poules\n";
+    
+    // Initialisation de wiringPi
+    if (wiringPiSetup() < 0) {
+        cout << "Impossible d'initialiser wiringPi\n";
+        return 1;
+    }
 
-	pinMode(pinRelai_1, OUTPUT);
-    pinMode(pinRelai_2, OUTPUT);
+    // Configuration des broches en sortie
+    pinMode(PIN_RELAI_1, OUTPUT);
+    pinMode(PIN_RELAI_2, OUTPUT);
 
-	usleep(100000);
+    // Séquence de test des relais
+    usleep(100000);
+    digitalWrite(PIN_RELAI_1, 1);
+    digitalWrite(PIN_RELAI_2, 1);
+    usleep(100000);
+    digitalWrite(PIN_RELAI_1, 0);
+    digitalWrite(PIN_RELAI_2, 0);
+    usleep(100000);
+    digitalWrite(PIN_RELAI_1, 1);
+    digitalWrite(PIN_RELAI_2, 1);
+    usleep(100000);
+    digitalWrite(PIN_RELAI_1, 0);
+    digitalWrite(PIN_RELAI_2, 0);
 
-	digitalWrite(pinRelai_1, 1);
-    digitalWrite(pinRelai_2, 1);
-	usleep(100000);
-	digitalWrite(pinRelai_1, 0);
-    digitalWrite(pinRelai_2, 0);
-	usleep(100000);
-	digitalWrite(pinRelai_1, 1);
-    digitalWrite(pinRelai_2, 1);
-	usleep(100000);
-	digitalWrite(pinRelai_1, 0);
-    digitalWrite(pinRelai_2, 0);
+    usleep(5000000);
 
-	usleep(5000000);
+    sql::Driver* driver;
+    sql::Connection* connexionBdd;
+    sql::Statement* stmt;
+    sql::ResultSet* res;
 
+    // Récupération du driver de la base de données
+    try {
+        driver = get_driver_instance();
+    }
+    catch (sql::SQLException e) {
+        cout << "Impossible de récupérer le driver bdd. Message d'erreur : " << e.what() << "\n";
+        return 1;
+    }
 
-	
-	sql::Driver* driver;
-	sql::Connection* connexion_bdd;
-	sql::Statement* stmt;
-	sql::ResultSet* res;
+    // Connexion à la base de données
+    try {
+        connexionBdd = driver->connect("tcp://ip:port", "login", "password");
+        connexionBdd->setSchema("GESTION_POULES");
+    }
+    catch (sql::SQLException e) {
+        cout << "Impossible de se connecter à la base de données. Message d'erreur : " << e.what() << "\n";
+        return 1;
+    }
 
-	try
-	{
-		driver = get_driver_instance();
-	}
-	catch (sql::SQLException e)
-	{
-		std::cout << "Impossible de récupérer le driver bdd. Message d'erreur : " << e.what() << "\n";
-		return 1;
-	}
+    stmt = connexionBdd->createStatement();
 
-	try
-    {
-		connexion_bdd = driver->connect("tcp://ip:port", "login", "password");
-		connexion_bdd->setSchema("GESTION_POULES");
-	}
-	catch (sql::SQLException e)
-	{
-		std::cout << "Impossible de se connecter à la base de données. Message d'erreur : " << e.what() << "\n";
-		return 1;
-	}
+    while (true) {
+        res = stmt->executeQuery("SELECT * FROM PORTES");
 
-	stmt = connexion_bdd->createStatement();
+        int compteurDebutJour = 0;
+        int compteurDebutNuit = 0;
+        int compteurActuel = 0;
+        int ouvert = 0;
+        int ferme = 0;
 
-	while(1){
-		res = stmt->executeQuery("SELECT * FROM PORTES");
+        cout << "\n";
 
-		int compteur_debut_jour = 0;
-		int compteur_debut_nuit = 0;
-		int compteur_actuel = 0;
-		int ouvert = 0;
-		int ferme = 0;
+        time_t datetimeActuel = time(0);
+        tm* ltm = localtime(&datetimeActuel);
+        char datetimeActuelFormate[30];
+        sprintf(datetimeActuelFormate, "%04d-%02d-%02d %02d:%02d:%02d", (1900 + ltm->tm_year), (1 + ltm->tm_mon), ltm->tm_mday, 1 + ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
 
-		std::cout << "\n";
+        compteurActuel = ((1 + ltm->tm_hour) * 60) + ltm->tm_min;
 
-		time_t datetime_actuel = time(0);
-		tm *ltm = localtime(&datetime_actuel);
-		char datetime_actuel_formate[30];
-		sprintf(datetime_actuel_formate, "%04d-%02d-%02d %02d:%02d:%02d", (1900 + ltm->tm_year), (1 + ltm->tm_mon), ltm->tm_mday, 1+ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
+        cout << "-----------------------------------------\n";
+        cout << "Datetime actuel : " << datetimeActuelFormate << "\n";
+        cout << "\nConfiguration récupérée de la bdd \n";
 
-		compteur_actuel = ((1+ltm->tm_hour)*60)+ltm->tm_min;
+        // Récupération des données de la base de données
+        while (res->next()) {
+            auto v = explode(':', res->getString("debut_jour"));
+            auto w = explode(' ', v[0]);
+            cout << "\t debut_jour : " << w[1] << ":" << v[1] << "\n";
+            compteurDebutJour = (stoi(w[1]) * 60) + stoi(v[1]);
 
-		std::cout << "-----------------------------------------\n";
-		std::cout << "Datetime actuel : " << datetime_actuel_formate << "\n";
-		std::cout << "\nConfiguration récupérée de la bdd \n";
-	
-		while (res->next()) {
-			auto v = explode(':', res->getString("debut_jour"));
-			auto w = explode(' ', v[0]);
-			std::cout << "\t debut_jour : " << w[1] << ":" << v[1] << "\n";
-			compteur_debut_jour = (stoi(w[1])*60)+stoi(v[1]);
+            auto x = explode(':', res->getString("debut_nuit"));
+            auto y = explode(' ', x[0]);
+            cout << "\t debut_nuit : " << y[1] << ":" << x[1] << "\n";
+            compteurDebutNuit = (stoi(y[1]) * 60) + stoi(x[1]);
 
-			auto x = explode(':', res->getString("debut_nuit"));
-			auto y = explode(' ', x[0]);
-			std::cout << "\t debut_nuit : " << y[1] << ":" << x[1] << "\n";
-			compteur_debut_nuit = (stoi(y[1])*60)+stoi(x[1]);
+            cout << "\t ouvert : " << res->getInt("ouvert") << "\n";
+            cout << "\t ferme : " << res->getInt("ferme") << "\n";
 
-			std::cout << "\t ouvert : " << res->getInt("ouvert") << "\n";
-			std::cout << "\t ferme : " << res->getInt("ferme") << "\n";
+            ouvert = res->getInt("ouvert");
+            ferme = res->getInt("ferme");
+        }
+        delete res;
 
-			ouvert = res->getInt("ouvert");
-			ferme = res->getInt("ferme");
-		}
-		delete res;
+        cout << "\ncompteurDebutJour : " << compteurDebutJour << "\n";
+        cout << "compteurDebutNuit : " << compteurDebutNuit << "\n";
+        cout << "compteurActuel : " << compteurActuel << "\n";
 
-		std::cout << "\ncompteur_debut_jour : " << compteur_debut_jour << "\n";
-		std::cout << "compteur_debut_nuit : " << compteur_debut_nuit << "\n";
-		std::cout << "compteur_actuel : " << compteur_actuel << "\n";
+        string phaseActuelle = "";
+        // Détermination de la phase actuelle (jour ou nuit)
+        if (compteurActuel < compteurDebutJour) {
+            phaseActuelle = "nuit";
+        }
+        else if (compteurActuel > compteurDebutJour && compteurActuel < compteurDebutNuit) {
+            phaseActuelle = "jour";
+        }
+        else if (compteurActuel > compteurDebutNuit) {
+            phaseActuelle = "nuit";
+        }
 
-		std::string phase_actuelle = "";
-		if(compteur_actuel < compteur_debut_jour){
-			phase_actuelle = "nuit";
-		}
-		else if(compteur_actuel > compteur_debut_jour && compteur_actuel < compteur_debut_nuit){
-			phase_actuelle = "jour";
-		}
-		else if(compteur_actuel > compteur_debut_nuit){
-			phase_actuelle = "nuit";
-		}
+        cout << "phase actuelle : " << phaseActuelle << "\n";
 
-		std::cout << "phase actuelle : " << phase_actuelle << "\n"; 
+        // Ouverture de la porte si c'est le jour et qu'elle n'est pas déjà ouverte
+        if (phaseActuelle == "jour" && ouvert == 0) {
+            ouvrirPorte();
+            stmt->execute("UPDATE PORTES SET ouvert=1, ferme=0 WHERE id=1");
+        }
 
-		if(phase_actuelle == "jour" && ouvert == 0){
-			ouverture_porte();
-			stmt->execute("UPDATE PORTES SET ouvert=1, ferme=0 WHERE id=1");
-		}
+        // Fermeture de la porte si c'est la nuit et qu'elle n'est pas déjà fermée
+        if (phaseActuelle == "nuit" && ferme == 0) {
+            fermerPorte();
+            stmt->execute("UPDATE PORTES SET ferme=1, ouvert=0 WHERE id=1");
+        }
 
-		if(phase_actuelle == "nuit" && ferme == 0){
-			fermeture_porte();
-			stmt->execute("UPDATE PORTES SET ferme=1, ouvert=0 WHERE id=1");
-		}
-		usleep(10000000);
-	}
-	delete connexion_bdd;
-	return 0;
+        usleep(10000000);
+    }
+
+    delete connexionBdd;
+    return 0;
 }
